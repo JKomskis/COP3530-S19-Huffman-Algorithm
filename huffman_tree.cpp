598ec0d7b903d2fbda3e57c57c948ac3f87712f1
JKomskis <joseph.komskis@gmail.com>
2019-02-14 22:00:18 -0500

    Added dummy return to get_character_code

--- huffman_tree.cpp
+++ huffman_tree.cpp
@@ -9,7 +9,7 @@ huffman_tree::~huffman_tree(){
 }
 
 std::string huffman_tree::get_character_code(char character) const {
-
+	return "";
 }
 
 std::string huffman_tree::encode(const std::string &file_name) const {
